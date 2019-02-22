631c66fa13e551b9c3ce6695bac602e5fcb3d8e2
kadamski <kadamski@grupazpr.pl>
2019-02-22 15:04:41 +0100

    poprawki README, path urls

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
-from django.contrib import admin
 
 urlpatterns = [
-    path('api/', include('movies.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('movies.urls')),
 ]
